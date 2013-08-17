111455cd3801dde07574d2e37e155a4e304d5c16
Drackenov <Drackenov@gmail.com>
2013-08-17 16:57:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gods::Application.config.secret_key_base = 'd4a0656d140e63c62e61b63a36ef4561c70617b9cd3154a9e6336b27d0063e3cabe89226769c7490a315dfb8b9ec55d7d6a168df2b24ce585ab576e73b5d5a77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gods::Application.config.session_store :cookie_store, key: '_gods_session'
